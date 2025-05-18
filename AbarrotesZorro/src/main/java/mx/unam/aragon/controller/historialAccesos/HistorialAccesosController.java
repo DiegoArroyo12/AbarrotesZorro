898a71abd3d3359501db98c1c9bf87f92aa9e060
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2025-05-17 18:26:02 -0600

    Historial de Accesos

---
+++ AbarrotesZorro/src/main/java/mx/unam/aragon/controller/historialAccesos/HistorialAccesosController.java
@@ -0,0 +1,32 @@
+package mx.unam.aragon.controller.historialAccesos;
+
+import mx.unam.aragon.model.entity.SucursalEntity;
+import mx.unam.aragon.repository.HistorialAccesoRepository;
+import mx.unam.aragon.repository.SucursalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HistorialAccesosController {
+
+    @Autowired
+    HistorialAccesoRepository historialAccesoRepository;
+
+    @Autowired
+    SucursalRepository sucursalRepository;
+
+    @GetMapping("empleados/historial_accesos")
+    public String mostrarAccesos(@RequestParam("idSucursal") String idSucursal, Model model) {
+        // Nombre de sucursal
+        String nombreSucursal = sucursalRepository.findById(Long.valueOf(idSucursal)).map(SucursalEntity::getNombre).orElse("Nombre no encontrado");
+
+        // Accesos
+        model.addAttribute("accesos", historialAccesoRepository.findBySucursalWithDetails(Integer.parseInt(idSucursal)));
+        model.addAttribute("nombreSucursal", nombreSucursal);
+        model.addAttribute("idSucursal", idSucursal);
+        return "historial_accesos";
+    }
+}
--- AbarrotesZorro/src/main/java/mx/unam/aragon/repository/HistorialAccesoRepository.java
+++ AbarrotesZorro/src/main/java/mx/unam/aragon/repository/HistorialAccesoRepository.java
@@ -4,7 +4,18 @@ import mx.unam.aragon.model.entity.EmpleadoEntity;
 import mx.unam.aragon.model.entity.HistorialAccesoEntity;
 import mx.unam.aragon.model.entity.InventarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface HistorialAccesoRepository extends JpaRepository<HistorialAccesoEntity,Long> {
     HistorialAccesoEntity findTopByEmpleadoOrderByFechaEntradaDesc(EmpleadoEntity empleado);
+
+    @Query("SELECT h FROM historial_accesos h " +
+            "JOIN FETCH h.empleado e " +
+            "LEFT JOIN FETCH h.caja c " +
+            "WHERE e.sucursal.id = :idSucursal " +
+            "ORDER BY h.fechaEntrada DESC")
+    List<HistorialAccesoEntity> findBySucursalWithDetails(@Param("idSucursal") Integer idSucursal);
 }
